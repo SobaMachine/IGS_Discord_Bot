839c65945cbb52712dd2220dcc7291dfd7cd050a
SobaMachine <ashley.e.kenn@gmail.com>
2022-02-23 15:24:51 -0500

    delete unused json file from old config
    commented on SinglePostChannel.py functions

--- ext/SinglePostChannel.py
+++ ext/SinglePostChannel.py
@@ -1,7 +1,7 @@
 from discord.utils import get
 from ext.FileUtils import get_config_value, Data
 
-
+# Changes user role on sending a message in specified channel
 async def single_post(_message):
     _discord_channel = await get_config_value(Data.col_voice_chan_id,Data.guilds_table,Data.col_guild_id,_message.author.guild.id)
     _role = await get_config_value(Data.col_intro_role_id,Data.guilds_table,Data.col_guild_id,_message.author.guild.id)
@@ -10,5 +10,10 @@ async def single_post(_message):
         role = get(member.guild.roles, id=_role)  # sets the role that will be given to the member
         await member.add_roles(role, reason="Made their introduction post")  # adds the role to the member and adds reason to audit log
 
+# Logic for getting birthday from user's post and adding it to our database for birthday reminders
 def set_birthday():
+    pass
+
+# Logic for getting birthday from database
+def get_birthday():
     pass
